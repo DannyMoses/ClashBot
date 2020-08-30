ab0fd9c28b9c50c8a4d442ba90faa585d5855b32
Daniel Moses <daniel.moses@stonybrook.edu>
2020-08-30 18:11:59 -0400

    Oops accidentally left API keys in plaintext

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from discord.ext import commands
 
-RIOT_API = 'RGAPI-db7a7e85-984b-440f-b444-d8184b3170ce'
-TOKEN = 'NzQ3NTkwODI0NjMxNzMwMjI2.X0RGNg.0h_zwy7Arw5jox1BShhqeh0Pxro'
 GUILD = 'Bot Testing'
 
 bot = commands.Bot(command_prefix="!")
